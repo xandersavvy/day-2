9607b870346e8b6aab790f7e8068f0cbe8c0d337
souvik ghosh <xandersavy@gmail.com>
2022-08-24 13:58:58 +0530

    OPen Single

--- cocktail-db/script.js
+++ cocktail-db/script.js
@@ -29,19 +29,21 @@ const alphabet = [
 
 let oldDrinks = [];
 let selectEl = document.getElementById("alp-select");
+let resEl = document.getElementById("res");
 
 alphabet.map((key) => {
   selectEl.innerHTML += `<option value="${key}">${key}</option>`;
 });
 
-function fetchCocktail(val = "a") {
+function fetchCocktail(val = "A") {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${val}`;
+  resEl.innerHTML = "Loading";
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       const { drinks } = data;
       oldDrinks = drinks;
-      generateHtml(drinks);
+      generateHtml(drinks, false);
     });
 }
 
@@ -51,16 +53,48 @@ function fetchNew() {
   fetchCocktail(selectEl.value);
 }
 
-function generateHtml(drinks = []) {
-  document.getElementById("res").innerHTML = "";
+function generateHtml(drinks = oldDrinks, ingred = false) {
+  resEl.innerHTML = "";
   if (drinks.length === 0) {
-    document.getElementById("res").innerHTML = "No results";
+    resEl.innerHTML = "No results";
     return;
   }
-  drinks.map((drink, index) => {
+
+  drinks.map((drink) => {
     const { strDrink, strCategory, strDrinkThumb } = drink;
-    document.getElementById("res").innerHTML += `
-    <div class="item">
+
+    //new code added
+
+    // for single with ingredient display
+    if (ingred) {
+      let p = "";
+      for (let i = 1; i < 16; i++) {
+        if (drink[`strIngredient${i}`] != null) {
+          p += `<li><h5>${drink[`strIngredient${i}`]} </h5><p> ${
+            drink[`strMeasure${i}`]
+          }</p></li>`;
+        }
+      }
+      resEl.innerHTML += `<div class="item-single" id="${strDrink}">
+        
+          <div class="thumb">
+            <img
+              src="${strDrinkThumb}"
+              alt=""
+              srcset=""
+            />
+          </div>
+          <button class="button button-primary" onclick="generateHtml()">back</button>
+           <p class="category">${strCategory}</p>
+            <h1 class="name">${strDrink}</h1>
+            <h3>Instruction:</h3>
+            <p>${drink.strInstructions}</p>
+            <h3>Ingredients:</h3>
+            <ul>${p}</ul>
+        </div>
+    `;
+    } else {
+      resEl.innerHTML += `<div class="item" id="${strDrink}" onclick="openSingle(this.id)">
           <div class="thumb">
             <img
               src="${strDrinkThumb}"
@@ -69,10 +103,12 @@ function generateHtml(drinks = []) {
             />
           </div>
            <p class="category">${strCategory}</p>
-            <h1 class="name">${strDrink}</p>
-        
+            <h1 class="name">${strDrink}</h1>
         </div>
     `;
+    }
+
+    //newcode complete
   });
 }
 
@@ -82,7 +118,6 @@ function search() {
   document.getElementById("search-field").value = "";
   if (val === "") return;
   val = val.toLowerCase();
-  console.log(val);
   let newArr = oldDrinks.filter((oldDrink) => {
     let drinkVal =
       oldDrink.strDrink.toLowerCase() +
@@ -91,4 +126,15 @@ function search() {
     return drinkVal.includes(val);
   });
   generateHtml(newArr);
+}
+
+//open single
+
+function openSingle(id) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      generateHtml(data.drinks, true);
+    });
 }
